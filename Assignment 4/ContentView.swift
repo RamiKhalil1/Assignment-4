970259bdfc5763e1f9c0f12bd5c0d51dd21678de
Lanslk <wolf810210@hotmail.com>
2024-10-09 11:48:39 +1100

    revert

--- Assignment 4/ContentView.swift
+++ Assignment 4/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
-        NavigationView {ffds
+        NavigationView {
             List {
                 ForEach(items) { item in
                     NavigationLink {
